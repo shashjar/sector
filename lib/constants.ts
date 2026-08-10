b8a5b8872d664b721ca3370a012055243b483f97
Shashank Jarmale <shashank.jarmale@sentry.io>
2026-08-10 11:37:22 -0700

    Zoom out default Sectional view a little bit

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,7 +5,4 @@ export const DEFAULT_AIRPORT = "KSQL";
 
 export const DEFAULT_CENTER = { lat: 37.51313, lon: -122.250838 };
 
-/**
- * Roughly 33 nm across on a desktop viewport.
- */
-export const DEFAULT_ZOOM = 11;
+export const DEFAULT_ZOOM = 10;
